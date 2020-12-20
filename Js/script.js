586b785b0dbb4bca7105cd2d642d85b42a396c3c
Mayukh Bhattacharya <mayukh1978@gmail.com>
2020-12-20 16:18:11 +0530

    other updates

--- Js/script.js
+++ Js/script.js
@@ -1,18 +1,20 @@
-fetch("https://indian-railway-live-train.p.rapidapi.com/live-train", {
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"x-rapidapi-key": "2ee3c77993msh920d38ecef85afep1ce417jsn1ffb06da3e54",
-		"x-rapidapi-host": "indian-railway-live-train.p.rapidapi.com"
-	},
-	"body": {
-		"trainID": "443",
-		"date": "05-10-2020"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+function getPnr() {
+	fetch("https://indian-railway-live-train.p.rapidapi.com/live-train", {
+		"method": "POST",
+		"headers": {
+			"content-type": "application/x-www-form-urlencoded",
+			"x-rapidapi-key": "2ee3c77993msh920d38ecef85afep1ce417jsn1ffb06da3e54",
+			"x-rapidapi-host": "indian-railway-live-train.p.rapidapi.com"
+		},
+		"body": {
+			"trainID": "443",
+			"date": "05-10-2020"
+		}
+	})
+		.then(response => {
+			console.log(response);
+		})
+		.catch(err => {
+			console.error(err);
+		});
+}
